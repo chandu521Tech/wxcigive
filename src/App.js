662cb127a3cc9cf06254eb0229d0a423f3f35ec7
Chandra Sekhar Reddy Karri <ckarri@microsoft.com>
2023-10-26 04:36:06 +0530

    added modal and cards data

--- src/App.js
+++ src/App.js
@@ -1,7 +1,24 @@
 import './App.css';
 
+import React, { useState } from 'react';
+
+import cardsData from "./cardsData.json";
+
 function App() {
+  const [showDialog, setShowDialog] = useState(false);
+  const [selectedCard, setSelectedCard] = useState(null); // cardsData?.cards[0
+  const handleCardClick = (cardIndex) => {
+    console.log(cardIndex, cardsData?.cards[cardIndex]);
+    setShowDialog(true);
+    setSelectedCard(cardsData?.cards[cardIndex]);
+    document.body.style.overflow = 'hidden';
+  }
+  const closeDialogClick = () => {
+    setShowDialog(false);
+    document.body.style.overflow = 'unset';
+  }
   return (
+    <>
     <div className="App">
       <header className="appHeader">
       </header>
@@ -11,21 +28,30 @@ function App() {
           <p>Bid to make it big! All auction bids to be donated on the Give Benevity Portal.</p>
         </div>
         <div className="cardsContainer">
-          <div className="card"></div>
-          <div className="card"></div>
-          <div className="card"></div>
-          <div className="card"></div>
-          <div className="card"></div>
-          <div className="card"></div>
-          <div className="card"></div>
-          <div className="card"></div>
-          <div className="card"></div>
-          <div className="card"></div>
-          <div className="card"></div>
-          <div className="card"></div>
+          {cardsData?.cards.length > 0 && cardsData?.cards.map((card, index) => {
+            return (<div className="card" onClick={(e) => handleCardClick(index)} key={card?.id} style={{backgroundImage: `url(${card?.thumbnail})`}}></div>);
+          })}
         </div>
       </main>
     </div>
+    {showDialog && (
+            <div className="popupContainer">
+            <div className="cardDetailsDialog">
+              <div className="cardImage" style={{backgroundImage: `url(${selectedCard?.thumbnail})`}}></div>
+              <div className="cardDetails">
+                <div className="auctionItem">Auction Item</div>
+                <h1 className="cardHeader">
+                  {selectedCard?.name}
+                </h1>
+                <div className="artBy">by&nbsp;{selectedCard?.author}</div>
+                <div className="description">{selectedCard?.description}</div>
+                <div className="giveSmallLogo"></div>
+              </div>
+              <div className="closeIcon" onClick={(e) => closeDialogClick()}></div>
+            </div>
+          </div>
+          )}
+    </>
   );
 }
 
